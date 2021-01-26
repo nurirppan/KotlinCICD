b2f71f3338678a2f1c598bd072b4cacd8eb75923
Nur Irfan Pangestu <nur.irfan@kompas.com>
2021-01-26 13:28:20 +0700

    + intro app center

--- app/src/main/java/com/nurirppan/kotlincicd/MainActivity.kt
+++ app/src/main/java/com/nurirppan/kotlincicd/MainActivity.kt
@@ -5,12 +5,51 @@ import androidx.appcompat.app.AppCompatActivity
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import com.nurirppan.kotlincicd.databinding.ActivityMainBinding
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         AppCenter.start(application, "55b80ff8-9bd9-4145-b54c-c51293ff0ac8", Analytics::class.java, Crashes::class.java)
+
+//        binding.tvHelloWord.setOnClickListener {
+//            //Crashes.generateTestCrash()
+//            Analytics.trackEvent("Test Track Event")
+//        }
+
+        binding.calculateButton.setOnClickListener {
+            // Crashes.generateTestCrash()
+            try {
+                val interestRate = binding.interestEditText.text.toString().toFloat()
+                val currentAge = binding.ageEditText.text.toString().toInt()
+                val retirementAge = binding.retirementEditText.text.toString().toInt()
+                val monthly = binding.monthlySavingsEditText.text.toString().toFloat()
+                val current = binding.currentEditText.text.toString().toFloat()
+
+                val properties:HashMap<String, String> = HashMap<String, String>()
+                properties.put("interest_rate", interestRate.toString())
+                properties.put("current_age", currentAge.toString())
+                properties.put("retirement_age", retirementAge.toString())
+                properties.put("monthly_savings", monthly.toString())
+                properties.put("current_savings", current.toString())
+
+                if (interestRate <= 0) {
+                    Analytics.trackEvent("wrong_interest_rate", properties)
+                }
+                if (retirementAge <= currentAge) {
+                    Analytics.trackEvent("wrong_age", properties)
+                }
+            } catch(ex: Exception){
+                Analytics.trackEvent(ex.message)
+            }
+        }
     }
 }
